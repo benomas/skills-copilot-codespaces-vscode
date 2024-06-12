088592e938efb4c34fea87b46b14dd27e556adf8
Beny <benomas@users.noreply.github.com>
2024-06-12 19:08:58 +0000

    =another js file added

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    var members = [
+        {name: 'John', skills: ['JavaScript', 'React']},
+        {name: 'Jane', skills: ['JavaScript', 'Angular']}
+    ];
+
+    return members;
+}
